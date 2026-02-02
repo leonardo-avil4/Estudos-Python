71c93fcab3b22416df12d46736c6a21d49724e40
leonardo-avil4 <leonardofabricioavila@gmail.com>
2026-02-02 15:32:53 -0400

    stop tracking .vscode folder

---
+++ 01-fundamentos-python/02-tipos-de-dados/aula01.py
@@ -0,0 +1,24 @@
+# O interpretador do Python + comentários de código.
+
+# Fluxo de execução do código:
+# → Da esquerda para direita e de cima para baixo.
+
+# Interpretador do Python:
+# → Faz a leitura/interpretação da linha de código de acordo com o fluxo de execução e o executa no mesmo instante.
+
+# O símbolo de cerquilha (#) é utilizado para fazer comentários (de única linha) no código. Comentários são ignorados pelo interpretador do python. Comentários são comumente utilizados para mostrar o que um determinado comando faz.
+
+
+# Função print → Exibe/Imprime uma saída no terminal. Imprimir na computação significa "mostrar algo na tela".
+print(123)
+
+
+# Docstrings como comentários
+# → Não são considerados como comentários pelo interpretador do python, mas é utilizado pelos desenvolvedores para fazer comentários de várias linhas.
+
+"""
+ DocString → Feita com aspas triplas (simples ou duplas). Devem ser abertas e fechadas com o mesmo tipo de aspas.
+
+ O interpretador do python faz a leitura de docstrings. Elas são armazenadas na memória.
+
+"""
